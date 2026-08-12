801079acb56950e83b15744433d9124b09f67da5
Arya <emailkamu@example.com>
2026-08-12 10:39:07 +0700

    fix: update skipsAuthorization signature to match Laravel Passport base class

--- app/Models/PassportClient.php
+++ app/Models/PassportClient.php
@@ -9,9 +9,11 @@ class PassportClient extends BaseClient
     /**
      * Determine if the client should skip the authorization prompt.
      *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  array  $scopes
      * @return bool
      */
-    public function skipsAuthorization()
+    public function skipsAuthorization(\Illuminate\Contracts\Auth\Authenticatable $user, array $scopes = []): bool
     {
         return true;
     }
